8932be98dd70f9b0ea06b29e9284acebe94a88a4
David Ugalde <daviduj@gmail.com>
2015-03-12 09:31:52 -0600

    Initial Commit

---
+++ MvcRealty/CassetteConfiguration.cs
@@ -0,0 +1,45 @@
+using Cassette;
+using Cassette.Scripts;
+using Cassette.Stylesheets;
+
+namespace MvcRealty
+{
+    /// <summary>
+    /// Configures the Cassette asset bundles for the web application.
+    /// </summary>
+    public class CassetteBundleConfiguration : IConfiguration<BundleCollection>
+    {
+        public void Configure(BundleCollection bundles)
+        {
+            // TODO: Configure your bundles here...
+            // Please read http://getcassette.net/documentation/configuration
+
+            // Make sure we don't pick up any SCSS files...
+            //var cssFilter = new FileSearch() { Pattern = "*.css" };
+
+            //// This default configuration treats each file as a separate 'bundle'.
+            //// In production the content will be minified, but the files are not combined.
+            //// So you probably want to tweak these defaults!
+            //// Stylesheets
+            ////bundles.AddPerIndividualFile<StylesheetBundle>("Content/styles/3rd", cssFilter, b => b.Media = "all");
+            ////bundles.AddPerIndividualFile<StylesheetBundle>("Content/styles/compiled", cssFilter, b => b.Media = "all");
+            bundles.AddPerSubDirectory<StylesheetBundle>("Content/styles/3rd");
+            bundles.AddPerSubDirectory<StylesheetBundle>("Content/styles/compiled");
+
+            //// Scripts
+            ////bundles.AddPerSubDirectory<ScriptBundle>("~/Scripts/3rd");
+            //bundles.Add<ScriptBundle>("~/Scripts/3rd/require.js");
+            //bundles.Add<ScriptBundle>("~/Scripts/compiled/common.js");
+            //bundles.AddPerSubDirectory<ScriptBundle>("~/Scripts/compiled");
+
+            // To combine files, try something like this instead:
+            //   bundles.Add<StylesheetBundle>("Content");
+            // In production mode, all of ~/Content will be combined into a single bundle.
+            
+            // If you want a bundle per folder, try this:
+            //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
+            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
+            // This is useful when there are lots of scripts for different areas of the website.
+        }
+    }
+}
---
+++ MvcRealty/Scripts/common/common.js
@@ -0,0 +1,27 @@
+require.config({
+  baseUrl: '/scripts/3rd',
+  paths: {
+    'jquery': '/scripts/3rd/jquery-1.10.2',
+    'knockout': '/scripts/3rd/knockout-3.3.0',
+    'kendo': '/scripts/3rd/kendo.all',
+    'ko-kendo': '/scripts/3rd/knockout-kendo.min'
+  },
+  shim: {
+    'jquery': {
+      init: function() {
+        return window.jQuery;
+      },
+      exports: '$'
+    },
+    'kendo': {
+      deps: ['jquery'],
+      init: function() {
+        return window.kendo;
+      }
+    },
+    'ko-kendo': {
+      deps: ['jquery', 'knockout', 'kendo'],
+      exports: 'ko-kendo'
+    }
+  }
+});
